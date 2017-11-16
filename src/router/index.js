4198811c9145572a83b84c9ac50c9a2dca5da55f
dllo <dllo@limingyu.local>
2017-11-16 17:48:26 +0800

    更新版本

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import YaoRoutes from './yaoguoqiang'
-import ZhangRoutes from '/zhangzeyu'
-import LiuRoutes from '/liuchunyang'
-import LiRoutes from '/limingyu'
+import YaoRoutes from './yaoguoqiang/index'
+import ZhangRoutes from './zhangzeyu'
+import LiuRoutes from './liuchunyang'
+import LiRoutes from './limingyu'
 
 Vue.use(Router)
 
---
+++ src/router/zhangzeyu/index.js
@@ -0,0 +1,10 @@
+
+import Home from '@/components/home/Home'
+
+export default [
+  {
+    path: '',
+    component: Home,
+    name: ''
+  }
+]
