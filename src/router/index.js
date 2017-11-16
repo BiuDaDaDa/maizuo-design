1cfccd0ba821886fe2d3d8001b9311bc324c2920
dllo <dllo@limingyu.local>
2017-11-16 17:17:06 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/home/Home'
+import YaoRoutes from './yaoguoqiang'
+import ZhangRoutes from '/zhangzeyu'
+import LiuRoutes from '/liuchunyang'
+import LiRoutes from '/limingyu'
 
 Vue.use(Router)
 
+let routers = YaoRoutes.concat(LiuRoutes).concat(ZhangRoutes).concat(LiRoutes)
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+  routes: routers
 })
