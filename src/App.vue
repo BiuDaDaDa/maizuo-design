9b429e600244cbf17955368bc76bcd327f999002
zhangzeyu007 <951642243@qq.com>
2017-11-17 09:56:20 +0800

    test:版本跟新

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,24 @@
   <div id="app">
 
     <router-view/>
+
   </div>
 </template>
 
 <script>
   export default {
     name: 'app'
-//    methods: {
-//      fecthHomeData () {
-//        this.$request({
-//          type: 'get',
-//          url: '',
-//          headers: {},
-//          params: {},
-//          success: function () {},
-//          failed: function () {}
-//        })
-//      }
-//    }
   }
 </script>
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    padding: 0;
+    margin: 0;
   }
 </style>
