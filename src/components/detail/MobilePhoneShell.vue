57457c52626356a90aca500bc63d156cb67087b4
dllo <dllo@DLH170707-13yaoguoqiang.local>
2017-11-22 14:19:46 +0800

    fix #3 by YaoGuoQiang:修补部分城市不能点击的问题

---
+++ src/components/detail/MobilePhoneShell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrap" v-html="msg">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MobilePhoneShell',
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    mounted () {
+      this.$request({
+        type: 'GET',
+        url: '/apt/active-detail?id=1190',
+        success: function (res) {
+          this.msg = res.data.data.data.content
+        },
+        failed: function (error) {
+          console.log(error)
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped lang="less">
+
+</style>
